e12fef3a7eff0a4300c06f8150d86c53a81029a9
Nishant kumar <nishantkumar@Nishants-MacBook-Pro.local>
2016-01-05 15:32:28 +0530

    specs for member sirectory

--- gcss_backup/pw_css_regrssion/tests/one.test.js
+++
@@ -1,5 +0,0 @@
-load("main_config.js");
-
-testOne("browserstack test for homepage on Windows 8.1 1176x2500 IE 11", function (driver, device) {
-    checkLayout(driver, "specs/welcomePage.spec", device.tags);
-});
